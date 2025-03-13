8a2096be69699e34f2e9d170c2bb9c1c4e506291
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-03-13 00:06:46 -0300

    ✨Adds auditing configuration and migrations
    Introduces auditing configuration file for defining audit behaviors.
    Creates migration for audits table to store audit records.
    Enables tracking of user actions and system changes.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     \App\Providers\AppServiceProvider::class,
     \App\Providers\AuthServiceProvider::class,
     \App\Providers\RepositoryServiceProvider::class,
+    \OwenIt\Auditing\AuditingServiceProvider::class,
 ];
